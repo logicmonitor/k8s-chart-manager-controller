a0ab9a45cb031c327d72e33c35e25dc38dfce80c
Jeff Wozniak <jeff.wozniak@logicmonitor.com>
2018-01-03 12:24:22 -0800

    fix health check

--- pkg/healthz/healthz.go
+++ pkg/healthz/healthz.go
@@ -2,15 +2,9 @@ package healthz
 
 import (
 	"net/http"
-
-	log "github.com/sirupsen/logrus"
 )
 
 // HandleFunc is an http handler function to expose health metrics.
 func HandleFunc(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	_, err := w.Write("ok")
-	if err != nil {
-		log.Errorf("Failed to write healthz: %v", err)
-	}
 }
