fb8b4f5c23e11d780a5fe2304d455721842bbedb
Jeff <woz5999@gmail.com>
2018-01-06 12:34:54 -0800

    break up validation rules func

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -46,79 +46,103 @@ func ChartMgrValidationRules() *apiextensionsv1beta1.CustomResourceValidation {
 				"spec",
 			},
 			Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-				"spec": {
-					Required: []string{
-						"chart",
+				"spec": specValidationRules(),
+			},
+		},
+	}
+}
+
+func specValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Required: []string{
+			"chart",
+		},
+		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+			"chart":   chartValidationRules(),
+			"values":  valuesValidationRules(),
+			"release": releaseValidationRules(),
+			"options": optionsValidationRules(),
+		},
+	}
+}
+
+func chartValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Required: []string{
+			"name",
+		},
+		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+			"name": {
+				Type:      "string",
+				MinLength: func(i int64) *int64 { return &i }(1),
+			},
+			"repository": repositoryValidationRules(),
+		},
+	}
+}
+
+func valuesValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Type: "array",
+		Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
+			Schema: &apiextensionsv1beta1.JSONSchemaProps{
+				Required: []string{
+					"name",
+					"value",
+				},
+				Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+					"name": {
+						Type:      "string",
+						MinLength: func(i int64) *int64 { return &i }(1),
 					},
-					Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-						"chart": {
-							Required: []string{
-								"name",
-							},
-							Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-								"name": {
-									Type:      "string",
-									MinLength: func(i int64) *int64 { return &i }(1),
-								},
-								"repository": {
-									Required: []string{
-										"name",
-										"url",
-									},
-									Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-										"name": {
-											Type:      "string",
-											MinLength: func(i int64) *int64 { return &i }(1),
-										},
-										"url": {
-											Type:    "string",
-											Pattern: ValidateChartRepoURLPattern,
-										},
-									},
-								},
-							},
-						},
-						"values": {
-							Type: "array",
-							Items: &apiextensionsv1beta1.JSONSchemaPropsOrArray{
-								Schema: &apiextensionsv1beta1.JSONSchemaProps{
-									Required: []string{
-										"name",
-										"value",
-									},
-									Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-										"name": {
-											Type:      "string",
-											MinLength: func(i int64) *int64 { return &i }(1),
-										},
-										"value": {
-											Type:      "string",
-											MinLength: func(i int64) *int64 { return &i }(1),
-										},
-									},
-								},
-							},
-						},
-						"release": {
-							Required: []string{
-								"name",
-							},
-							Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-								"name": {
-									Type:    "string",
-									Pattern: ValidateReleaseNamePattern,
-								},
-							},
-						},
-						"options": {
-							Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
-								"createOnly": {
-									Type: "boolean",
-								},
-							},
-						},
+					"value": {
+						Type:      "string",
+						MinLength: func(i int64) *int64 { return &i }(1),
 					},
 				},
+			},
+		},
+	}
+}
+
+func releaseValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Required: []string{
+			"name",
+		},
+		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+			"name": {
+				Type:    "string",
+				Pattern: ValidateReleaseNamePattern,
+			},
+		},
+	}
+}
+
+func optionsValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+			"createOnly": {
+				Type: "boolean",
+			},
+		},
+	}
+}
+
+func repositoryValidationRules() *apiextensionsv1beta1.JSONSchemaProps {
+	return &apiextensionsv1beta1.JSONSchemaProps{
+		Required: []string{
+			"name",
+			"url",
+		},
+		Properties: map[string]apiextensionsv1beta1.JSONSchemaProps{
+			"name": {
+				Type:      "string",
+				MinLength: func(i int64) *int64 { return &i }(1),
+			},
+			"url": {
+				Type:    "string",
+				Pattern: ValidateChartRepoURLPattern,
 			},
 		},
 	}
